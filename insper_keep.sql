260d8ebabda8310baccc38a3a87e258220255e44
Fogelman <ddfogelman@gmail.com>
2018-08-26 21:11:15 -0300

    Adding basic login page and MYsql basic structure

---
+++ insper_keep.sql
@@ -0,0 +1,25 @@
+create database if not exists insper_keep;
+use insper_keep;
+
+
+CREATE TABLE user (
+ user_id INT NOT NULL AUTO_INCREMENT,
+ login VARCHAR(20) NOT NULL,
+ password VARCHAR(64) NOT NULL,
+ nome_completo VARCHAR(100),
+ email VARCHAR(50);
+ img BLOB;
+ PRIMARY KEY (user_id)
+);
+
+CREATE TABLE nota (
+ nota_id INT NOT NULL AUTO_INCREMENT,
+ nota TEXT NOT NULL,
+ person_id INT,
+ email VARCHAR(50),
+ active INT,
+ PRIMARY KEY (nota_id),
+ FOREIGN KEY(person_id ) REFERENCES user(user_id)
+ 
+);
+
